8091838e66920012c9ee15fb440baabcf9a5adcb
ethil12 <126690974+ethil12@users.noreply.github.com>
2023-03-02 10:45:13 -0500

    Add files via upload

---
+++ VISUAL/js/menu.js
@@ -0,0 +1,15 @@
+const menuInicial = document.querySelector('.menu'); //tomando la clase menu
+const menuFinal = document.querySelector('.menu-navegacion');
+
+console.log(menuInicial);
+console.log(menuFinal);
+
+menuInicial.addEventListener('click', ()=>{    //al elemento se le agrega un evento .addEventListener de tipo click 
+    menuFinal.classList.toggle("spread")       // al menu -le seleccione las clases -y que coloque y le quite la clase spread
+})
+
+window.addEventListener('clcik', e=>{
+    if(menuFinal.classList.contains('spread') && e.target != menuFinal && e.target !=menuInicial){
+        menuFinal.classList.toggle("spread")
+    }
+})
